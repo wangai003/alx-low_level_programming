ac4c1f989163553cb2648a19684a6e5bacbb0b2f
wangai003 <wangai003@gmail.com>
2023-02-16 14:25:34 -0800

    Write a C program that prints

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -Write a C program that prints with printf
+ * return 1 (success)
+ */
+
+int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+
+return (1);
+}
