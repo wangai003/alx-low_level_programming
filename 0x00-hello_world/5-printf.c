04e0a527635d5714e8cbddaf5e2f65baf349d7f7
wangai003 <wangai003@gmail.com>
2023-02-16 13:23:49 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A program that prints using printf 
+ * return : 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
