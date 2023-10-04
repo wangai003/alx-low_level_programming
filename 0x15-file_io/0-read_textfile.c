6d52d143d4533049f6956bc57fc1a8f99140a496
wangai003 <wangaimoses003@gmail.com>
2023-10-04 05:23:00 -0700

    run 4

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,31 +1,29 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * read_textfile- Custom text file reader and writer.
- * @filename: Name of the text file to read.
- * @letters: Number of letters to be processed.
- *
- * Return: The actual number of bytes read and printed.
- *         0 when the function fails or filename is NULL.
+ * read_textfile - Read text file and print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w - actual number of bytes read and printed
+ *         0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *custom_buf;
-	ssize_t custom_fd;
-	ssize_t custom_write;
-	ssize_t custom_total;
+    char *buf;
+    ssize_t fd;
+    ssize_t w;
+    ssize_t t;
 
-	custom_fd = open(filename, O_RDONLY);
-	if (custom_fd == -1)
-		return (0);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
+    buf = malloc(sizeof(char) * letters);
+    t = read(fd, buf, letters);
+    w = write(STDOUT_FILENO, buf, t);
 
-	custom_buf = malloc(sizeof(char) * letters);
-	custom_total = read(custom_fd, custom_buf, letters);
-	custom_write = write(STDOUT_FILENO, custom_buf, custom_total);
-
-	free(custom_buf);
-	close(custom_fd);
-	return (custom_write);
+    free(buf);
+    close(fd);
+    return (w);
 }
 
